c5f6ac036bbfb99708666eb1ae2de0c873cdec68
jenjnif <jenjnif@hotmail.com>
2018-01-23 16:37:05 +0000

    addition of functions from old version

--- TextAdventure00Funv2.py
+++ TextAdventure00Funv2.py
@@ -11,28 +11,50 @@ def process_input_for_name(user_input):
 	print('Welcome ' + user_input + '. You are ' + indefinite_article + " " + character_generated)
 	return 1
 
-def process_input_for_woods(user_input):
-	if user_input.lower() == "left":
-		print("You chose to go left...")
-		return 1
-	elif user_input.lower() == "right":
-		print("You chose to go right...")
+def process_input_for_climb_walk(user_input):
+	if user_input.lower() == "c":
+		print("Excellent work. You have a much better view from up here.")
 		return 2
+	elif user_input.lower() == "w":
+		print("You come across a clearing and in the distance you see a pair of dogs.")
+		return
 	else:
 		print("Invalid input")
 		return None
 
-def process_input_for_building(user_input):
-	if user_input.lower() == "yes":
-		return 2
-	elif user_input.lower() == "no":
+def process_input_for_tree_view(user_input):
+	if user_input.lower() == "1":
+		return 3
+	elif user_input.lower() == "2":
 		print("OK... stay here")
 		return None
 	else:
 		print("Invalid input")
 		return None
 
-def process_input_for_school(user_input):
+def process_input_for_inspect(user_input):
+	if user_input.lower() == "yes":
+		print("GAME OVER... stay in school kids")
+		exit()
+	elif user_input.lower() == "no":
+		print("You win at life")
+		exit()
+	else:
+		print("Invalid input")
+		return None
+
+def process_input_for_pickup(user_input):
+	if user_input.lower() == "yes":
+		print("GAME OVER... stay in school kids")
+		exit()
+	elif user_input.lower() == "no":
+		print("You win at life")
+		exit()
+	else:
+		print("Invalid input")
+		return None
+
+def process_input_for_inspect(user_input):
 	if user_input.lower() == "yes":
 		print("GAME OVER... stay in school kids")
 		exit()
@@ -48,17 +70,23 @@ scenes = [{
 	"input_text": "Please tell me your name.",
 	"process": process_input_for_name
 },{
-	"instructions": "You are the woods",
-	"input_text": "Do you want to go right or left?",
-	"process": process_input_for_woods
+	"instructions": "You are lost in the Wild Woods of Woowoowoo.""There are many magic stones in the forest. These could be helpful to you if you want to get home quickly but be careful how you use them.",
+	"input_text": "You are surrounded by trees, perhaps you could climb up to get a better view? Type c to climb or w to keep on walking.",
+	"process": process_input_for_climb_walk
 },{
-	"instructions": "You are in a building",
-	"input_text": "Do you want to open the door?",
-	"process": process_input_for_building
+	"instructions": "You can see your home over in the distance and the nice, safe, winding path you took to get you here " +
+		"but you will never get back if go the long way round. You are going to have to go through the deep forest past " +
+		"many obstacles to get back.",
+	"input_text": "Type 1 to climb down or 2 to stay in the tree.",
+	"process": process_input_for_tree_view
 },{
-	"instructions": "You are in a school",
-	"input_text": "Do you want to leave?",
-	"process": process_input_for_school
+	"instructions": "When you get down from the tree you see a something shiny a few metres away.",
+	"input_text": "Would you like to go over to the object? Yes/no?",
+	"process": process_input_for_inspect
+},{
+	"instructions": "When you get down from the tree you see a something shiny a few metres away.",
+	"input_text": "Would you like to go over to the object? Yes/no?",
+	"process": process_input_for_pickup
 }]
 
 
